9ce238b93a7370bca6d99b89a5d0ee36c6f45007
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-17 09:29:43 -0400

    Create base.jl

---
+++ src/base.jl
@@ -0,0 +1,31 @@
+
+#
+# Usando os dados de apoios, monta o vetor de gls livres do problema
+#
+function Free_DOFs(bmesh::Bmesh,nebc::Int64,ebc::Matrix{Float64})
+  
+  # Dimensão da malha
+  nnos = bmesh.nn
+
+  # Se for 2D ou 3D mudamos o número de gls por nó
+  dim = 2
+  if isa(bmesh,Bmesh3D)
+    dim=3
+  end
+
+  # Gerar um vetor de 1 até dim*nnos
+  todos = collect(1:dim*nnos)
+   
+  # Olha quem está sendo preso e coloca um -1 no lugar
+  for i=1:nebc
+       no = Int(ebc[i,1])
+       gl = Int(ebc[i,2])
+       todos[dim*(no-1)+gl] = -1
+  end     
+   
+  # Pega todos os valores que são maiores do que zero
+  livres = todos[todos.>0]
+
+  return livres, length(livres)
+     
+end
