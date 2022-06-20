c7e1644e82f523eb283eae6faa2ab2f549dbb3ca
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-20 17:44:36 -0400

    add Plots as dependency and export plot for Mesh

--- src/LMesh.jl
+++ src/LMesh.jl
@@ -3,16 +3,19 @@ module LMesh
    # Dependency
    using BMesh
    using LinearAlgebra
+   using Plots
 
    # Local includes
    include("material.jl")
    include("geometry.jl")
    include("mesh.jl")
    include("base.jl")
+   include("show.jl")
 
    # Exports
    export Material, Geometry
    export Mesh, Mesh2D, Mesh3D
-   export Free_DOFs #, Plot_structure
+   export Free_DOFs 
+   export plot
 
 end # module LMesh
