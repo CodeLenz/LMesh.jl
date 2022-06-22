77195741b47e84a1ba54bec31dc70aa7466ae139
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-22 16:44:45 -0400

    Adding some checks for consistency

--- src/geometry.jl
+++ src/geometry.jl
@@ -10,7 +10,11 @@ struct Geometry
     thickness::Float64
 
     # Default constructor
-    function Geometry(;A=0.0,thickness=0.0)
+    function Geometry(;A=1.0,thickness=1.0)
+
+        # Test consistency
+        A>0.0 || throw("Geometry::A should be > 0") 
+        thickness>0.0 || throw("Geometry:: thickness  should be > 0") 
 
         # create type
         new(A,thickness)
