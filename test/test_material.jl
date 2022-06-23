539eed55289a8f8e2bae12827dac648729004f84
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-23 09:45:31 -0400

    Adding tests to density

--- test/test_material.jl
+++ test/test_material.jl
@@ -4,10 +4,16 @@
     @test isa(Material(Ex=100.0),Material)
     @test isa(Material(Ex=100.0,νxy=0.3),Material)
     @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16),Material)
+    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0),Material)
+    
 
     # Should Trhow
-    @test_throws String Material()
     @test_throws String Material(Ex=-1.0)
+    @test_throws String Material(Ex=0.0)
+    
+    @test_throws String Material(density=-1.0)
+    @test_throws String Material(density=0.0)
+    
     
     
 end
