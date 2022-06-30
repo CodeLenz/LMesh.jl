eadb9dafe9af1948a0a91c648fc331b8e0a2103b
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-30 17:10:41 -0400

    Update LMesh.jl

--- src/LMesh.jl
+++ src/LMesh.jl
@@ -8,17 +8,19 @@ module LMesh
 
    # Local includes
 
-   include("mesh.jl")
+
    include("material.jl")
    include("geometry.jl")
+   include("mesh.jl")
    
    include("overload_bmesh.jl")
    include("base.jl")
    include("show.jl")
 
    # Exports
-   export Mesh, Mesh2D, Mesh3D
    export Material, Geometry
+   export Mesh, Mesh2D, Mesh3D
+   
    export Conec,Coord,Length,DOFs,T_matrix
    export Free_DOFs, Nodal_coordinates, Centroid
    export plot
