5c5dd046846d84e4936c0091d4166cd6e984aebe
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-06-30 20:33:19 -0400

    Add tests for model and custom

--- test/test_material.jl
+++ test/test_material.jl
@@ -5,7 +5,10 @@
     @test isa(Material(Ex=100.0,νxy=0.3),Material)
     @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16),Material)
     @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0),Material)
-    
+    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0,model=:EPT),Material)
+    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0,model=:EPD),Material)
+    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0,model=:Custom,ones(2,2),Material)
+   
 
     # Should Trhow
     @test_throws String Material(Ex=-1.0)
@@ -13,6 +16,12 @@
     
     @test_throws String Material(density=-1.0)
     @test_throws String Material(density=0.0)
+   
+    @test_throws String Material(model=:bla)
+    
+    @test_throws String Material(custom=Int64[])
+    
+    @test_throws String Material(model=:Custom)
     
     
     
