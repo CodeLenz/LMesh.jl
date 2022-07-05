f7cb6d804fe1f49a069d71003ade3f0e86b9d375
CodeLenz <eduardobarplenz@gmail.com>
2022-07-05 09:42:37 -0400

    Add tests for loadcases (Free_DOFs with loadcases) Add tests for consistency check in both ebc and nbc (new test case)

--- test/test_mesh.jl
+++ test/test_mesh.jl
@@ -1,4 +1,4 @@
-@testset "LMesh" begin
+@testset "LMesh - no loadcase" begin
 
     ####################    2D BMesh ######################
     #
@@ -168,3 +168,367 @@
     @test_throws String Mesh3D(b3,materials,geometries,ebc,nbc,geo_ele=geo_ele)
     
 end
+
+
+@testset "LMesh - loadcases" begin
+
+       ####################    2D BMesh ######################
+       #
+       #    Valid inputs (no error) 
+       #
+       nn = 4
+       ne = 6
+       coord = [0.0 0.0 ;
+                1.0 0.0 ;
+                1.0 1.0 ;
+                0.0 1.0 ]
+       connect = [1 2 ; 2 3 ; 3 4 ; 4 1; 1 3 ; 4 2]
+       Lx = 1.0
+       Ly = 1.0
+       nx = 1
+       ny = 1
+       etype = :truss2D
+   
+       # truss2D
+       b2 = Bmesh2D(etype,nn,ne,coord,connect,Lx,Ly,nx,ny)
+   
+       # Essential boundary conditions
+       ebc = [1 1 0.0 1; 
+              2 2 0.0 1; 
+              3 2 0.0 1; 
+              4 1 0.0 1;
+              1 1 0.0 2; 
+              2 2 0.0 2; 
+              3 2 0.0 2]
+
+
+       nbc = [1 2 -100.0 1;
+              2 2 +200.0 2 ] 
+    
+       materials = [Material(Ex=1.0), Material(Ex=1.0,density=7000.0) ]
+       geometries = [Geometry(A=1.0), Geometry(A=2.0), Geometry(A=3.0)]
+   
+       # Valid tests
+       @test isa(Mesh2D(b2,materials,geometries,ebc,nbc),Mesh2D)
+   
+       m2 = Mesh2D(b2,materials,geometries,ebc,nbc)
+       @test m2.nload == 2
+       @test m2.ngls[1]== 8 - 5
+       @test m2.ngls[2]== 8 - 4
+
+
+       # Some invalid tests
+
+       # EBC
+
+       # Invalid node (-1)
+       Xebc = [-1 1 0.0 1;  # -1 < 0
+              2 2 0.0 1; 
+              3 2 0.0 1; 
+              4 1 0.0 1;
+              1 1 0.0 2; 
+              2 2 0.0 2; 
+              3 2 0.0 2]
+       @test_throws String Mesh2D(b2,materials,geometries,Xebc,nbc)
+
+
+       # Invalid node (>nn)
+       Xebc = [1 1 0.0 1; 
+              2 2 0.0 1; 
+              5 2 0.0 1; #<- 5 > 4
+              4 1 0.0 1;
+              1 1 0.0 2; 
+              2 2 0.0 2; 
+              3 2 0.0 2]
+       @test_throws String Mesh2D(b2,materials,geometries,Xebc,nbc)
+
+       # Invalid node (0)
+       Xebc = [1 1 0.0 1; 
+              2 2 0.0 1; 
+              0 2 0.0 1; #<- 
+              4 1 0.0 1;
+              1 1 0.0 2; 
+              2 2 0.0 2; 
+              3 2 0.0 2]
+       @test_throws String Mesh2D(b2,materials,geometries,Xebc,nbc)
+
+
+        # Invalid dof
+        Xebc = [1 -1 0.0 1;  # -1 < 0
+        2 2 0.0 1; 
+        3 2 0.0 1; 
+        4 1 0.0 1;
+        1 1 0.0 2; 
+        2 2 0.0 2; 
+        3 2 0.0 2]
+       @test_throws String Mesh2D(b2,materials,geometries,Xebc,nbc)
+
+       # Invalid dof
+       Xebc = [1 0 0.0 1;  # 0
+       2 2 0.0 1; 
+       3 2 0.0 1; 
+       4 1 0.0 1;
+       1 1 0.0 2; 
+       2 2 0.0 2; 
+       3 2 0.0 2]
+       @test_throws String Mesh2D(b2,materials,geometries,Xebc,nbc)
+
+       # Invalid dof
+       Xebc = [1 3 0.0 1;  # 3 > 2
+              2 2 0.0 1; 
+              3 2 0.0 1; 
+              4 1 0.0 1;
+              1 1 0.0 2; 
+              2 2 0.0 2; 
+              3 2 0.0 2]
+      @test_throws String Mesh2D(b2,materials,geometries,Xebc,nbc)
+
+       # Invalid loadcase
+       Xebc = [1 2 0.0 -1;   # -1
+              2 2 0.0 1; 
+              3 2 0.0 1; 
+              4 1 0.0 1;
+              1 1 0.0 2; 
+              2 2 0.0 2; 
+              3 2 0.0 2]
+      @test_throws String Mesh2D(b2,materials,geometries,Xebc,nbc)
+
+      # NBC
+
+      # Invalid node
+      Xnbc = [10 2 -100.0 1;   # 10 
+      2 2 +200.0 2 ] 
+      @test_throws String Mesh2D(b2,materials,geometries,ebc,Xnbc)
+
+      # Invalid node
+      Xnbc = [0 2 -100.0 1;   # 0
+      2 2 +200.0 2 ] 
+      @test_throws String Mesh2D(b2,materials,geometries,ebc,Xnbc)
+
+      # Invalid node
+      Xnbc = [1 2 -100.0 1;   
+      -1 2 +200.0 2 ]         # -1
+      @test_throws String Mesh2D(b2,materials,geometries,ebc,Xnbc)
+
+      # Invalid dof
+      Xnbc = [1 0 -100.0 1;   # 0
+      2 2 +200.0 2 ] 
+      @test_throws String Mesh2D(b2,materials,geometries,ebc,Xnbc)
+
+      # Invalid dof
+      Xnbc = [1 -1 -100.0 1;   # -1
+      2 2 +200.0 2 ] 
+      @test_throws String Mesh2D(b2,materials,geometries,ebc,Xnbc)
+
+      # Invalid dof
+      Xnbc = [1 2 -100.0 1;   
+      2 3 +200.0 2 ]         # 3
+      @test_throws String Mesh2D(b2,materials,geometries,ebc,Xnbc)
+
+      # Invalid loadcase
+      Xnbc = [1 1 -100.0 1;   
+      2 2 +200.0 -1 ] #-1
+      @test_throws String Mesh2D(b2,materials,geometries,ebc,Xnbc)
+
+
+      ####################   3D BMesh ######################
+      #
+      #    Valid inputs (no error) 
+      #
+      nn = 8
+      ne = 12
+      coord = [0.0 0.0 0.0;
+               1.0 0.0 0.0;
+                1.0 1.0 0.0;
+                0.0 1.0 0.0;
+                0.0 0.0 1.0;
+                1.0 0.0 1.0;
+                1.0 1.0 1.0;
+                0.0 1.0 1.0    
+                ]
+       connect = [1 2 ; 2 3 ; 3 4 ; 4 1;
+                  5 6 ; 6 7 ; 7 8 ; 8 5;
+                  1 5; 2 6; 3 7; 4 8]
+       Lx = 1.0
+       Ly = 1.0
+       Lz = 1.0
+       nx = 1
+       ny = 1
+       nz = 1
+       etype = :truss3D
+   
+       # truss3D
+       b3 = Bmesh3D(etype,nn,ne,coord,connect,Lx,Ly,Lz,nx,ny,nz)
+   
+       # Essential boundary conditions
+       ebc = [1 1 0.0 1; 
+              1 3 0.0 1;  
+              2 2 0.0 1;
+              5 1 0.0 1; 
+              7 3 0.0 1; 
+              8 1 0.0 1;
+              1 1 0.0 2; 
+              1 3 0.0 2;  
+              2 2 0.0 2;
+              5 1 0.0 2; 
+              7 3 0.0 2]
+   
+       nbc = [1 2 -100.0 1;
+              2 2 +200.0 2 ] 
+    
+       materials = [Material(Ex=1.0), Material(Ex=1.0,density=7000.0) ]
+       geometries = [Geometry(A=1.0), Geometry(A=2.0), Geometry(A=3.0)]
+          
+       # Valid tests
+       @test isa(Mesh3D(b3,materials,geometries,ebc,nbc),Mesh3D)
+           
+       m3 =  Mesh3D(b3,materials,geometries,ebc,nbc)
+       @test m3.nload == 2
+
+       @test m3.ngls[1]== 24 - 6
+       @test m3.ngls[2]== 24 - 5
+
+       # INVALID TESTS
+
+       # EBC
+
+       # Invalid node   (-1)
+       Xebc = [1 1 0.0 1; 
+              1 3 0.0 1;  
+              2 2 0.0 1;
+              5 1 0.0 1; 
+              7 3 0.0 1; 
+              -1 1 0.0 1;  #<-
+              1 1 0.0 2; 
+              1 3 0.0 2;  
+              2 2 0.0 2;
+              5 1 0.0 2; 
+              7 3 0.0 2]
+       Mesh3D(b3,materials,geometries,Xebc,nbc)
+
+
+       # Invalid node   (0)
+       Xebc = [1 1 0.0 1; 
+              1 3 0.0 1;  
+              2 2 0.0 1;
+              5 1 0.0 1; 
+              7 3 0.0 1; 
+              8 1 0.0 1;  
+              1 1 0.0 2; 
+              0 3 0.0 2;   #<-
+              2 2 0.0 2;
+              5 1 0.0 2; 
+              7 3 0.0 2]
+       Mesh3D(b3,materials,geometries,Xebc,nbc)
+
+       # Invalid node   (>nn)
+       Xebc = [1 1 0.0 1; 
+              1 3 0.0 1;  
+              2 2 0.0 1;
+              5 1 0.0 1; 
+              7 3 0.0 1; 
+              8 1 0.0 1;  
+              1 1 0.0 2; 
+              1 3 0.0 2;   
+              2 2 0.0 2;
+              5 1 0.0 2; 
+              10 3 0.0 2]  #<-
+       Mesh3D(b3,materials,geometries,Xebc,nbc)
+
+
+       # Invalid dof   (-1)
+       Xebc = [1 1 0.0 1; 
+           1 3 0.0 1;  
+           2 2 0.0 1;
+           5 1 0.0 1; 
+           7 -1 0.0 1; #<-
+           8 1 0.0 1;  
+           1 1 0.0 2; 
+           1 3 0.0 2;  
+           2 2 0.0 2;
+           5 1 0.0 2; 
+           7 3 0.0 2]
+       Mesh3D(b3,materials,geometries,Xebc,nbc)
+
+       # Invalid dof   (0)
+       Xebc = [1 1 0.0 1; 
+           1 3 0.0 1;  
+           2 2 0.0 1;
+           5 1 0.0 1; 
+           7 0 0.0 1; #<-
+           8 1 0.0 1;  
+           1 1 0.0 2; 
+           1 3 0.0 2;  
+           2 2 0.0 2;
+           5 1 0.0 2; 
+           7 3 0.0 2]
+       Mesh3D(b3,materials,geometries,Xebc,nbc)
+
+       # Invalid dof   (4>3)
+       Xebc = [1 1 0.0 1; 
+           1 3 0.0 1;  
+           2 2 0.0 1;
+           5 1 0.0 1; 
+           7 4 0.0 1; #<-
+           8 1 0.0 1;  
+           1 1 0.0 2; 
+           1 3 0.0 2;  
+           2 2 0.0 2;
+           5 1 0.0 2; 
+           7 3 0.0 2]
+       Mesh3D(b3,materials,geometries,Xebc,nbc)
+
+       # Invalid loadcase  (-1)
+       Xebc = [1 1 0.0 1; 
+           1 3 0.0 1;  
+           2 2 0.0 1;
+           5 1 0.0 1; 
+           7 3 0.0 1; 
+           8 1 0.0 1;  
+           1 1 0.0 -1; <-
+           1 3 0.0 2;  
+           2 2 0.0 2;
+           5 1 0.0 2; 
+           7 3 0.0 2]
+       Mesh3D(b3,materials,geometries,Xebc,nbc)
+
+       # NBC
+
+       # Invalid node (-1)
+       Xnbc = [1 2 -100.0 1;
+       -1 2 +200.0 2 ]   #<-
+       Mesh3D(b3,materials,geometries,ebc,Xnbc)
+
+       # Invalid node (0)
+       Xnbc = [0 2 -100.0 1; #<-
+       2 2 +200.0 2 ]   
+       Mesh3D(b3,materials,geometries,ebc,Xnbc)
+
+       # Invalid node (>nn)
+       Xnbc = [10 2 -100.0 1; #<-
+       2 2 +200.0 2 ]   
+       Mesh3D(b3,materials,geometries,ebc,Xnbc)
+
+       # Invalid dof (-1)
+       Xnbc = [1 -1 -100.0 1; #<-
+       2 2 +200.0 2 ]   
+       Mesh3D(b3,materials,geometries,ebc,Xnbc)
+
+       # Invalid dof (0)
+       Xnbc = [1 0 -100.0 1; #<-
+       2 2 +200.0 2 ]   
+       Mesh3D(b3,materials,geometries,ebc,Xnbc)
+
+       # Invalid dof (4)
+       Xnbc = [1 4 -100.0 1; #<-
+       2 2 +200.0 2 ]   
+       Mesh3D(b3,materials,geometries,ebc,Xnbc)
+
+       # Invalid loadcase
+       Xnbc = [1 3 -100.0 -1; #<-
+       2 2 +200.0 2 ]   
+       Mesh3D(b3,materials,geometries,ebc,Xnbc)
+
+
+   end
+   
