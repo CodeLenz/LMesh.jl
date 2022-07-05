dddd604e71352d71f490dfe85a81fd48d3c69aef
CodeLenz <eduardobarplenz@gmail.com>
2022-07-05 11:18:54 -0400

    Fiz test for Free_DOFs (missing loadcase)

--- test/test_base.jl
+++ test/test_base.jl
@@ -85,7 +85,7 @@
            8 1 0.0 ]
 
     # Free_DOFs 
-    gls, ngls =  Free_DOFs(b3,nebc,ebc)
+    gls, ngls =  Free_DOFs(b3,nebc,ebc,1)
 
     # Reference values
     # 1 2 3   4 5 6   7 8 9   10 11 12   13 14 15   16 17 18  19 20 21  22 23 24
@@ -97,7 +97,7 @@
     @test all(gls.==refer)
 
     # Check inferred
-    @isinferred Free_DOFs(b3,nebc,ebc)
+    @isinferred Free_DOFs(b3,nebc,ebc,1)
 
     #
     # Nodal coordinates
