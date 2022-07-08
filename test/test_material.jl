2fffc0aaa26abc2bbdb0f9d565a47d1fba2b9475
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-07-08 08:14:06 -0400

    Add tests for new fields

--- test/test_material.jl
+++ test/test_material.jl
@@ -1,14 +1,33 @@
 @testset "Material" begin
 
     # Valid tests
+    
+    # Just #Ex
     @test isa(Material(Ex=100.0),Material)
+
+    # Ex and Poisson
     @test isa(Material(Ex=100.0,νxy=0.3),Material)
-    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16),Material)
-    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0),Material)
-    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0,model=:EPT),Material)
-    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0,model=:EPD),Material)
-    @test isa(Material(Ex=100.0,νxy=0.3,α=1E-16,density=7850.0,model=:Custom,custom=ones(2,2)),Material)
+
+    # Ex, Poisson and thermal expansion
+    @test isa(Material(Ex=100.0,νxy=0.3,α_T=1E-16),Material)
+
+    # Ex, Poisson and thermal expansion, density
+    @test isa(Material(Ex=100.0,νxy=0.3,α_T=1E-16,density=7850.0),Material)
+
+    # Ex, Poisson and thermal expansion, density + model
+    @test isa(Material(Ex=100.0,νxy=0.3,α_T=1E-16,density=7850.0,model=:EPT),Material)
+    @test isa(Material(Ex=100.0,νxy=0.3,α_T=1E-16,density=7850.0,model=:EPD),Material)
+    @test isa(Material(Ex=100.0,νxy=0.3,α_T=1E-16,density=7850.0,model=:Custom,custom=ones(2,2)),Material)
    
+    # Limit stress
+    @test isa(Material(Ex=100.0,limit_stress=1E6),Material)
+
+    # Proportional Damping
+    # Limit stress
+    @test isa(Material(Ex=100.0,limit_stress=1E6,α_c=1E-6),Material)
+    @test isa(Material(Ex=100.0,limit_stress=1E6,β_c=1E-6),Material)
+    @test isa(Material(Ex=100.0,limit_stress=1E6,β_c=1E-6,α_c=1E-6),Material)
+    
 
     # Should Trhow
     @test_throws String Material(Ex=-1.0)
@@ -18,7 +37,9 @@
     @test_throws String Material(density=0.0)
    
     @test_throws String Material(model=:bla)
-    
+
+    @test_throws String Material(Ex=100.0,limit_stress=-1E6)
+    @test_throws String Material(Ex=100.0,limit_stress=0.0)
     
     
 end
