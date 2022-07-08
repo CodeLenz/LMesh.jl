c95897d39b0d93450baa45f1f312cfd891fbf266
Eduardo Lenz <eduardobarplenz@gmail.com>
2022-07-08 09:27:06 -0300

    Revert proportional damping
    It is a global property for the mesh

--- src/material.jl
+++ src/material.jl
@@ -5,7 +5,7 @@
 Basic structure to store material data
 
     Material(;Ex=1.0,νxy=0.0, α_T=0.0, density=1.0, model=:EPT,
-              limit_stress=1.0, α_c=0.0,β_c=0.0,
+              limit_stress=1.0,
               custom=[1.0 1.0 ; 1.0 1.0])
 
 where all inputs are named. 
@@ -16,8 +16,6 @@ Ex = Young's mudule (in X) [Pa]
 density = material density [Kg/m³]
 model =  2D elasticity (:EPT or :EPD) or :Custom 
 limit_stress = limit stress (von-Mises criteria) [Pa]
-α_c = Mass parameter for proportional damping
-β_c = Stiffness parameter for proportional damping
     
 Custom is a custom constitutive Matrix given 
 by the user. 
@@ -44,17 +42,12 @@ struct Material
     # Limit stress
     limit_stress::Float64
 
-    # Parameters for proportional damping
-    # C = α_cM + _cK
-    α_c::Float64
-    β_c::Float64
-
     # Custom constitutive matrix
     custom::Matrix{Float64}
     
     # Default constructor
     function Material(;Ex=1.0,νxy=0.0, α_T=0.0, density=1.0, 
-                       limit_stress=1.0, α_c=0.0, β_c=0.0,
+                       limit_stress=1.0, 
                        model=:EPT, custom=[1.0 1.0 ; 1.0 1.0])
 
         # Basic tests
@@ -68,7 +61,7 @@ struct Material
         end
         
         # create type
-        new(Ex,νxy,α,density,model,limit_stress,α_c,β_c,custom)
+        new(Ex,νxy,α_T,density,model,limit_stress,custom)
 
     end
 
